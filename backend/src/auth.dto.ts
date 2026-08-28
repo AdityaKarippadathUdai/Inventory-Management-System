447bb0dc41ddd228f680be708bf35f0fa1cd06c5
Aditya Karippadath Udai <adityaudai.1305@gmail.com>
2026-08-28 21:46:45 +0530

    Add auth DTOs

---
+++ backend/src/auth.dto.ts
@@ -0,0 +1,38 @@
+import { IsBoolean, IsEmail, IsOptional, IsString, Matches, MinLength } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail() email!: string;
+  @IsString() @MinLength(1) password!: string;
+}
+
+export class ChangePasswordDto {
+  @IsString() currentPassword!: string;
+  @IsString() @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{10,}$/, {
+    message: 'Password must be at least 10 characters with upper, lower, number, and special character',
+  }) password!: string;
+}
+
+export class ForgotPasswordDto { @IsEmail() email!: string; }
+
+export class ResetPasswordDto {
+  @IsString() token!: string;
+  @IsString() @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{10,}$/) password!: string;
+}
+
+export class UpdateProfileDto { @IsString() @MinLength(2) name!: string; }
+
+export class CreateUserDto {
+  @IsString() @MinLength(2) name!: string;
+  @IsEmail() email!: string;
+  @IsString() @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{10,}$/) password!: string;
+  @IsString() role!: string;
+}
+
+export class UpdateUserDto {
+  @IsOptional() @IsString() @MinLength(2) name?: string;
+  @IsOptional() @IsEmail() email?: string;
+  @IsOptional() @IsString() role?: string;
+  @IsOptional() @IsBoolean() isActive?: boolean;
+}
+
+export class StatusDto { @IsBoolean() isActive!: boolean; }
