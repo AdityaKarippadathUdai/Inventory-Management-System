0d5b2a294b3c9328966fb303649e1ac23e46a445
Aditya Karippadath Udai <adityaudai.1305@gmail.com>
2026-08-28 21:26:45 +0530

    Update backend app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthModule } from './health/health.module';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { HealthModule } from './health/health.module.js';
 
 @Module({
   imports: [HealthModule],
