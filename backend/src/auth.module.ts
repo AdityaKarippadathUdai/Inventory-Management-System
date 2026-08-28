e2182c0e204e3585a5b92732a0c3bbb84f68c74e
Aditya Karippadath Udai <adityaudai.1305@gmail.com>
2026-08-28 21:46:55 +0530

    Add auth module

---
+++ backend/src/auth.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+import { JwtAuthGuard, PermissionGuard } from './auth.guards.js';
+import { PrismaService } from './prisma.service.js';
+
+@Module({
+  imports: [JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService, JwtAuthGuard, PermissionGuard],
+  exports: [AuthService, JwtAuthGuard, PermissionGuard, PrismaService],
+})
+export class AuthModule {}
