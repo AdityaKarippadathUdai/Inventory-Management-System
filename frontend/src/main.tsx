84d385fd70090cf9c0482cf4ed846c0184e99fa2
Aditya Karippadath Udai <adityaudai.1305@gmail.com>
2026-08-28 21:46:12 +0530

    Update frontend entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// @ts-ignore
 import './index.css'
 import App from './App.tsx'
 
