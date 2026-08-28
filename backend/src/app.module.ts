4f596705f19656d80eaadf0a260735dbfd3ec0ee
Aditya Karippadath Udai <adityaudai.1305@gmail.com>
2026-08-28 21:53:15 +0530

    Add MasterDataModule to AppModule imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth.module.js';
 import { UsersController } from './users.controller.js';
 import { UsersService } from './users.service.js';
 import { PrismaService } from './prisma.service.js';
+import { MasterDataModule } from './master-data.module.js';
 
 @Module({
-  imports: [HealthModule, AuthModule],
+  imports: [HealthModule, AuthModule, MasterDataModule],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService, PrismaService],
 })
