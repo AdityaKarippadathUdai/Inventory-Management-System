14409137a40bd2a3e3171a73c3d5f81dea51481e
Aditya Karippadath Udai <adityaudai.1305@gmail.com>
2026-08-28 21:44:55 +0530

    Update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { HealthModule } from './health/health.module.js';
+import { AuthModule } from './auth.module.js';
+import { UsersController } from './users.controller.js';
+import { UsersService } from './users.service.js';
+import { PrismaService } from './prisma.service.js';
 
 @Module({
-  imports: [HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthModule, AuthModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService, PrismaService],
 })
 export class AppModule {}
