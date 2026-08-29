ffaf95bfca67802942fb3351c5bc6d166dbae418
Aditya Karippadath Udai <adityaudai.1305@gmail.com>
2026-08-29 21:20:13 +0530

    Add ReservationsModule for managing reservations functionality

---
+++ backend/src/reservations.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth.module.js';
+import { ReservationsController } from './reservations.controller.js';
+import { ReservationsService } from './reservations.service.js';
+
+@Module({ imports: [AuthModule], controllers: [ReservationsController], providers: [ReservationsService], exports: [ReservationsService] })
+export class ReservationsModule {}
